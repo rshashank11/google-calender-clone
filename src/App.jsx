65280d1d428c1a5878d0280c7d418131aaa41f5f
Shashank <shashankyadav2000@gmail.com>
2023-12-28 19:34:59 +0530

    Added time picker component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Calender from './components/Calender'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns/AdapterDateFns"
 
 const App = () => {
   return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
     <div><Calender /></div>
+    </LocalizationProvider>
   )
 }
 
