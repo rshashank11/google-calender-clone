4c3e803aa4c37b7f11c80ecb46b14d8df27cc4f2
Shashank <shashankyadav2000@gmail.com>
2023-12-29 21:00:10 +0530

    Added select component and added colors

--- src/components/ColorPallete.jsx
+++ src/components/ColorPallete.jsx
@@ -1,38 +1,60 @@
-import React from 'react'
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import React, { useState } from "react";
 
 const ColorPallete = () => {
+  const [color, setColor] = useState("tomato");
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
   return (
-    <div className="row left flex gap-4">
-                <input
-                style={{accentColor: "blue", WebkitAppearance: "none",}}
-                  type="radio"
-                  name="color"
-                  value="blue"
-                  id="blue"
-                  
-                  className="color-radio w-4 h-4 appearance-none rounded-full border border-blue checked:bg-blue"
-                />
-                
-                <input
-                style={{accentColor: "red", WebkitAppearance: "none",}}
-                  type="radio"
-                  name="color"
-                  value="red"
-                  id="red"
-                  className="color-radio w-4 h-4 appearance-none rounded-full border border-red checked:bg-red"
-                />
-                
-                <input
-                style={{accentColor: "green", WebkitAppearance: "none"}}
-                  type="radio"
-                  name="color"
-                  value="green"
-                  id="green"
-                  className="color-radio w-4 h-4 appearance-none rounded-full border border-green checked:bg-green"
-                />
-                
-              </div>
-  )
-}
+    <>
+      <div>
+        <FormControl></FormControl>
+        <InputLabel id="color-select">Color</InputLabel>
+        <Select
+          value={color}
+          sx={{ minWidth: 10 }}
+          id="color-select"
+          label="Color"
+          onChange={handleColorChange}
+        >
+          <MenuItem value="tomato">
+            <div className="min-w-4 min-h-4 rounded-full bg-tomato"></div>
+          </MenuItem>
+          <MenuItem value="flamino">
+            <div className="min-w-4 min-h-4 rounded-full bg-flamingo"></div>
+          </MenuItem>
+          <MenuItem value="tangerine">
+            <div className="min-w-4 min-h-4 rounded-full bg-tangerine"></div>
+          </MenuItem>
+          <MenuItem value="banana">
+            <div className="min-w-4 min-h-4 rounded-full bg-banana"></div>
+          </MenuItem>
+          <MenuItem value="sage">
+            <div className="min-w-4 min-h-4 rounded-full bg-sage"></div>
+          </MenuItem>
+          <MenuItem value="basil">
+            <div className="min-w-4 min-h-4 rounded-full bg-basil"></div>
+          </MenuItem>
+          <MenuItem value="peacock">
+            <div className="min-w-4 min-h-4 rounded-full bg-peacock"></div>
+          </MenuItem>
+          <MenuItem value="blueberry">
+            <div className="min-w-4 min-h-4 rounded-full bg-blueberry"></div>
+          </MenuItem>
+          <MenuItem value="lavender">
+            <div className="min-w-4 min-h-4 rounded-full bg-lavender"></div>
+          </MenuItem>
+          <MenuItem value="grape">
+            <div className="min-w-4 min-h-4 rounded-full bg-grape"></div>
+          </MenuItem>
+          <MenuItem value="graphite">
+            <div className="min-w-4 min-h-4 rounded-full bg-graphite"></div>
+          </MenuItem>
+        </Select>
+      </div>
+    </>
+  );
+};
 
-export default ColorPallete
+export default ColorPallete;
