2c1563c579cc5613c8750f5f6867acab0bb9e366
Shashank <shashankyadav2000@gmail.com>
2023-12-29 20:59:43 +0530

    Updated open and close functionalities of modal

--- src/components/CalenderDay.jsx
+++ src/components/CalenderDay.jsx
@@ -1,21 +1,58 @@
-import { endOfDay, format, isBefore, isSameMonth, isSameWeek, isToday } from 'date-fns'
-import React from 'react'
-import EventsModal from './EventsModal'
+import {
+  endOfDay,
+  format,
+  isBefore,
+  isSameMonth,
+  isSameWeek,
+  isToday,
+} from "date-fns";
+import React, { useState } from "react";
+import EventsModal from "./EventsModal";
 
-const CalenderDay = ({day, firstDay}) => { 
-  
+const CalenderDay = ({ day, firstDay }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
   return (
-    <div className={`day bg-white p-1 overflow-hidden flex flex-col ${!isSameMonth(day, new Date()) && "non-month-day bg-opacity-75 opacity-75"} `}>
-            <div className={`day-header mb-1 flex flex-col items-center relative `}> 
-            
-             {isSameWeek(firstDay, day) && <div className="week-name uppercase text-xs font-bold text-[#777]">{format(day, "eee")}</div>} 
+    <>
+      <EventsModal
+        day={day}
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      >
+        <div
+          onClick={openModal}
+          className={`day bg-white p-1 overflow-hidden flex flex-col ${
+            !isSameMonth(day, new Date()) &&
+            "non-month-day bg-opacity-75 opacity-75"
+          } `}
+        >
+          <div
+            className={`day-header mb-1 flex flex-col items-center relative `}
+          >
+            {isSameWeek(firstDay, day) && (
+              <div className="week-name uppercase text-xs font-bold text-[#777]">
+                {format(day, "eee")}
+              </div>
+            )}
 
-              <div className={`day-number text-sm w-6 h-6 flex justify-center items-center ${isToday(day) && "bg-blueBg rounded-[50%] text-white"} ${isBefore(endOfDay(day), new Date()) && "old-month-day opacity-50"}`}>{format(day, "d")}</div>
-
-              <EventsModal day={day}/>
+            <div
+              className={`day-number text-sm w-6 h-6 flex justify-center items-center ${
+                isToday(day) && "bg-blueBg rounded-[50%] text-white"
+              } ${
+                isBefore(endOfDay(day), new Date()) &&
+                "old-month-day opacity-50"
+              }`}
+            >
+              {format(day, "d")}
             </div>
           </div>
-  )
-}
+        </div>
+      </EventsModal>
+    </>
+  );
+};
 
-export default CalenderDay
+export default CalenderDay;
--- src/components/EventsModal.jsx
+++ src/components/EventsModal.jsx
@@ -1,34 +1,20 @@
 import { format } from "date-fns";
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+
 import ReactModal from "react-modal";
 import "../App.css";
-import ColorPallete from "./ColorPallete";
 import EventForm from "./EventForm";
 
-const EventsModal = ({ day }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  function openModal() {
-    setIsModalOpen(true);
-  }
-
-  function closeModal() {
-    setIsModalOpen(false);
-  }
+const EventsModal = ({ day, isModalOpen, setIsModalOpen, children }) => {
   return (
     <>
-      <button
-        className="add-event-btn opacity-0 absolute bg-none border-none rounded-[50%] w-6 h-6 flex justify-center items-center right-0 top-0 text-xl cursor-pointer text-[#333] hover:bg-[#f1f3f4] hover:opacity-75"
-        onClick={openModal}
-      >
-        +
-      </button>
+      {children}
       <ReactModal
         className="modal"
         overlayClassName="overlay"
         parentSelector={() => document.querySelector("#root")}
         isOpen={isModalOpen}
-        onRequestClose={closeModal}
+        onRequestClose={() => setIsModalOpen(false)}
         preventScroll={true}
         contentLabel="Modal"
         ariaHideApp={false}
@@ -37,7 +23,10 @@ const EventsModal = ({ day }) => {
           <div className="modal-title">
             <div className="modal-title h-10 bg-borderColor flex items-center justify-between px-4 text-grayTwo">
               <h1>{format(day, "iiii, MMMM d")}</h1>
-              <button className="close-btn text-2xl" onClick={closeModal}>
+              <button
+                className="close-btn text-2xl"
+                onClick={() => setIsModalOpen(false)}
+              >
                 &times;
               </button>
             </div>
