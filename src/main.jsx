3ce743d357239e1b26ed247c51e3a6c9fbdbb212
Shashank <shashankyadav2000@gmail.com>
2024-01-02 23:11:24 +0530

    Removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './utils/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./utils/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>,
-)
+  </Provider>
+);
